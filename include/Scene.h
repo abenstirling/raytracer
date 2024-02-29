f0338c47ee94abb6c08ad3def7ebe651f802be5c
Nicco Pompili <niccoep@Niccos-MacBook-Air.local>
2024-02-29 14:03:30 -0800

    idk

--- include/Scene.h
+++ include/Scene.h
@@ -6,6 +6,12 @@ public:
     ~Scene();
 
 private:
-    
+    /**
+     * TODO
+     * [x] project setup
+     * [ ] port readfile from hw2
+     *      stack stuff (local to world coords)
+     * [ ] do the ray tracing
+    */
 
 };
