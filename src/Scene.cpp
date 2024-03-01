2209b90a1cf4f312e9d1e894434c087dbc51e596
Ben Stirling <abenstirling@pm.me>
2024-02-29 20:39:28 -0800

    Added objects

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -1,9 +1,25 @@
 #include "Scene.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
 
-Scene::Scene(){
+struct Camera {
+  float eye[3];
+  float lookat[3];
+  float up[3];
+  float fov;
+};
 
-}
+Scene::Scene() {}
 
-Scene::~Scene(){
-    
-}
+Scene::~Scene() {}
+
+Scene::load(const char *filename) {
+  std::ifstream file(filename);
+  std::string line;
+
+  while (std::getline(file, line)) {
+    if (line.empty() || line[0] == '#')
+      continue;
+    parseLine(line);
+  }
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,27 @@
 
 #include <iostream>
 
-#include "My_math.h"
+#include "../include/My_math.h"
+#include "../include/Scene.h"
 
+int main(int argc, char *argv[]) {
 
-int main(int argc, char* argv[]){
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " <scene_file>" << std::endl;
+    return 1;
+  }
 
+  Scene::load(argv[1]);
 
-    mm::mat2 m(0.0, 0.1,
-               1.0, 1.1);
-    mm::print_mat(m);
-    m(0,0) = 2.0;
-    mm::print_mat(m);
+  mm::mat2 m(0.0, 0.1, 1.0, 1.1);
+  mm::print_mat(m);
+  m(0, 0) = 2.0;
+  mm::print_mat(m);
 
-    mm::vec2 v(1.0);
-    mm::print_vec(v);
-    v(1) = 9.0;
-    mm::print_vec(v);
+  mm::vec2 v(1.0);
+  mm::print_vec(v);
+  v(1) = 9.0;
+  mm::print_vec(v);
 
-    return 0;
+  return 0;
 }
-
